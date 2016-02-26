a055349331f7c20d6ef8e31339c53b1301640c01
Finnian Anderson <finnian@fxapi.co.uk>
2016-02-26 20:21:19 +0000

    Update becausePythonDeservesAPlace.py

--- becausePythonDeservesAPlace.py
+++ becausePythonDeservesAPlace.py
@@ -1,10 +1,10 @@
 from awesomeness import Tom
 from bad import Oli
-Tom.say('Wow, we should use python')
-Oli.say('No lets use a bad language, NODE!')
-Oli.kill_idea('NODE')
-Oli.say('Yes we should use python!')
+
+while True:
+  Tom.say('Wow, we should use python')
+  Oli.say('No lets use a bad language, NODE!')
+  Oli.kill_idea('NODE')
+  Oli.say('Yes we should use python!')
 
 #Life Complete
-
-
