844399592e07cb817a8df76a511d831ab2c56295
Popey Gilbert <popey@debenclipper.com>
2016-02-26 21:44:35 +0000

    updates

--- app.js
+++ app.js
@@ -2,26 +2,37 @@ var app = require('express')()
 var http = require('http').Server(app)
 var io = require('socket.io')(http)
 
-var shortURLPromise
-  , short = require('short');
- 
-// connect to mongodb 
-short.connect('mongodb://localhost/short');
+var shortURLPromise,
+  short = require('short')
+
+short.connect('mongodb://localhost/short')
+
+function lookup (long, callback) {
+  short.retrieve(long).then(function (result) {
+    callback(result.URL)
+  })
+}
+
+app.get('/:long', function (req, res) {
+  lookup(req.params.long, function (redirect) {
+    console.log('Short: ' + redirect)
+    res.redirect('http://' + redirect)
+  })
+})
 
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html')
 })
 
 io.on('connection', function (socket) {
-  console.log("Someone connected")
   socket.on('url', function (msg) {
-    var shortURLPromise = short.generate({ URL : msg })
-    
-    shortURLPromise.then(function(mongodbDoc) {
-      console.log("Long: " + mongodbDoc.URL)
-      console.log("Short: " + mongodbDoc.hash)
+    var shortURLPromise = short.generate({ URL: msg })
+
+    shortURLPromise.then(function (mongodbDoc) {
+      console.log('Long: ' + mongodbDoc.URL)
+      console.log('Short: ' + mongodbDoc.hash)
       io.emit('short', msg)
-      short.retrieve(mongodbDoc.hash).then(function(result) {
+      short.retrieve(mongodbDoc.hash).then(function (result) {
         io.emit('long', result.hash)
       })
     })
