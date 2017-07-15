34264621f0b6d64eb6a25e0486fba7d792b9cda9
Finnian Anderson <get@finnian.io>
2017-07-15 12:50:14 +0100

    Add release scripts for building docker images
    Required because Docker hub doesn’t yet support multi stage Dockerfiles
    for automated builds 😭

---
+++ release.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# build images & push to hub
+
+TAG=`node -p "require('./package.json').version"`
+IMAGE="subjectrefresh/uptimebot"
+
+echo "Building $IMAGE:$TAG"
+docker build -t $IMAGE:$TAG . && \
+docker tag $IMAGE:$TAG $IMAGE:latest && \
+docker push $IMAGE:$TAG && \
+docker push $IMAGE:latest
