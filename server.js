1a4344a30ca6101ffa709ee8aca813d8f554384e
Finnian Anderson <get@finnian.io>
2017-08-05 13:14:08 +0100

    Redirect old links with 301

--- server.js
+++ server.js
@@ -62,7 +62,7 @@ app.get('/s', (req, res, next) => {
   return res.status(301).redirect('/')
 })
 
-app.get(['/s/:short', '/:short'], (req, res, next) => {
+app.get('/s/:short', (req, res, next) => {
   models.Url.findOne({ short: req.params.short }, (err, doc) => {
     if (err) return next(err)
     if (!doc) return next()
@@ -73,6 +73,10 @@ app.get(['/s/:short', '/:short'], (req, res, next) => {
       if (err) console.error(err)
     })
   })
+})
+
+app.get('/:short', (req, res, next) => {
+  return res.status(301).redirect('/s/' + req.params.short)
 })
 
 app.use(function(req, res, next) {
